5ecf7057431132f6424f5821c3bd5f55ba7ee107
sunshinejen <35351160+sunshinejen@users.noreply.github.com>
2018-12-02 00:09:17 -0800

    Creates collab.sh

---
+++ collab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Collaborators Assignment
+# ITC136 - Fall 2018
+# Eddy Matlock-Mahon
+# Jennifer Villacis
+# Marcus Price
+
+echo "My name is Jen and I have 3 dogs, Dottie, Tank, and Rocky"
